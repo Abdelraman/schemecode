99b6db6b27df8459b404f57448e93d47d2a550d4
Abdelrahman Eid <a.eid502@gmail.com>
2019-04-14 00:16:33 -0800

    removing blog posts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,7 +76,7 @@
                     <li class="scroll"><a href="#about-us">About Us</a></li>
                     <li class="scroll"><a href="#portfolio">Portfolio</a></li>
                     <li class="scroll"><a href="#team">Team</a></li>
-                    <li class="scroll"><a href="#blog">Blog</a></li>
+                    {{--<li class="scroll"><a href="#blog">Blog</a></li>--}}
                     <li class="scroll"><a href="#contact">Contact</a></li>
                 </ul>
             </div>
@@ -407,102 +407,102 @@
     {{--</div>--}}
 {{--</section><!--/#twitter-->--}}
 
-<section id="blog">
-    <div class="container">
-        <div class="row">
-            <div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp" data-wow-duration="1200ms" data-wow-delay="300ms">
-                <h2>Blog Posts</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam</p>
-            </div>
-        </div>
-        <div class="blog-posts">
-            <div class="row">
-                <div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">
-                    <div class="post-thumb">
-                        <a href="#"><img class="img-responsive" src="images/blog/1.jpg" alt=""></a>
-                        <div class="post-meta">
-                            <span><i class="fa fa-comments-o"></i> 3 Comments</span>
-                            <span><i class="fa fa-heart"></i> 0 Likes</span>
-                        </div>
-                        <div class="post-icon">
-                            <i class="fa fa-pencil"></i>
-                        </div>
-                    </div>
-                    <div class="entry-header">
-                        <h3><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h3>
-                        <span class="date">June 26, 2014</span>
-                        <span class="cetagory">in <strong>Photography</strong></span>
-                    </div>
-                    <div class="entry-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    </div>
-                </div>
-                <div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="600ms">
-                    <div class="post-thumb">
-                        <div id="post-carousel" class="carousel slide" data-ride="carousel">
-                            <ol class="carousel-indicators">
-                                <li data-target="#post-carousel" data-slide-to="0" class="active"></li>
-                                <li data-target="#post-carousel" data-slide-to="1"></li>
-                                <li data-target="#post-carousel" data-slide-to="2"></li>
-                            </ol>
-                            <div class="carousel-inner">
-                                <div class="item active">
-                                    <a href="#"><img class="img-responsive" src="images/blog/2.jpg" alt=""></a>
-                                </div>
-                                <div class="item">
-                                    <a href="#"><img class="img-responsive" src="images/blog/1.jpg" alt=""></a>
-                                </div>
-                                <div class="item">
-                                    <a href="#"><img class="img-responsive" src="images/blog/3.jpg" alt=""></a>
-                                </div>
-                            </div>
-                            <a class="blog-left-control" href="#post-carousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-                            <a class="blog-right-control" href="#post-carousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
-                        </div>
-                        <div class="post-meta">
-                            <span><i class="fa fa-comments-o"></i> 3 Comments</span>
-                            <span><i class="fa fa-heart"></i> 0 Likes</span>
-                        </div>
-                        <div class="post-icon">
-                            <i class="fa fa-picture-o"></i>
-                        </div>
-                    </div>
-                    <div class="entry-header">
-                        <h3><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h3>
-                        <span class="date">June 26, 2014</span>
-                        <span class="cetagory">in <strong>Photography</strong></span>
-                    </div>
-                    <div class="entry-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    </div>
-                </div>
-                <div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="800ms">
-                    <div class="post-thumb">
-                        <a href="#"><img class="img-responsive" src="images/blog/3.jpg" alt=""></a>
-                        <div class="post-meta">
-                            <span><i class="fa fa-comments-o"></i> 3 Comments</span>
-                            <span><i class="fa fa-heart"></i> 0 Likes</span>
-                        </div>
-                        <div class="post-icon">
-                            <i class="fa fa-video-camera"></i>
-                        </div>
-                    </div>
-                    <div class="entry-header">
-                        <h3><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h3>
-                        <span class="date">June 26, 2014</span>
-                        <span class="cetagory">in <strong>Photography</strong></span>
-                    </div>
-                    <div class="entry-content">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                    </div>
-                </div>
-            </div>
-            <div class="load-more wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="500ms">
-                <a href="#" class="btn-loadmore"><i class="fa fa-repeat"></i> Load More</a>
-            </div>
-        </div>
-    </div>
-</section><!--/#blog-->
+{{--<section id="blog">--}}
+    {{--<div class="container">--}}
+        {{--<div class="row">--}}
+            {{--<div class="heading text-center col-sm-8 col-sm-offset-2 wow fadeInUp" data-wow-duration="1200ms" data-wow-delay="300ms">--}}
+                {{--<h2>Blog Posts</h2>--}}
+                {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ut enim ad minim veniam</p>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="blog-posts">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="400ms">--}}
+                    {{--<div class="post-thumb">--}}
+                        {{--<a href="#"><img class="img-responsive" src="images/blog/1.jpg" alt=""></a>--}}
+                        {{--<div class="post-meta">--}}
+                            {{--<span><i class="fa fa-comments-o"></i> 3 Comments</span>--}}
+                            {{--<span><i class="fa fa-heart"></i> 0 Likes</span>--}}
+                        {{--</div>--}}
+                        {{--<div class="post-icon">--}}
+                            {{--<i class="fa fa-pencil"></i>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="entry-header">--}}
+                        {{--<h3><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h3>--}}
+                        {{--<span class="date">June 26, 2014</span>--}}
+                        {{--<span class="cetagory">in <strong>Photography</strong></span>--}}
+                    {{--</div>--}}
+                    {{--<div class="entry-content">--}}
+                        {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="600ms">--}}
+                    {{--<div class="post-thumb">--}}
+                        {{--<div id="post-carousel" class="carousel slide" data-ride="carousel">--}}
+                            {{--<ol class="carousel-indicators">--}}
+                                {{--<li data-target="#post-carousel" data-slide-to="0" class="active"></li>--}}
+                                {{--<li data-target="#post-carousel" data-slide-to="1"></li>--}}
+                                {{--<li data-target="#post-carousel" data-slide-to="2"></li>--}}
+                            {{--</ol>--}}
+                            {{--<div class="carousel-inner">--}}
+                                {{--<div class="item active">--}}
+                                    {{--<a href="#"><img class="img-responsive" src="images/blog/2.jpg" alt=""></a>--}}
+                                {{--</div>--}}
+                                {{--<div class="item">--}}
+                                    {{--<a href="#"><img class="img-responsive" src="images/blog/1.jpg" alt=""></a>--}}
+                                {{--</div>--}}
+                                {{--<div class="item">--}}
+                                    {{--<a href="#"><img class="img-responsive" src="images/blog/3.jpg" alt=""></a>--}}
+                                {{--</div>--}}
+                            {{--</div>--}}
+                            {{--<a class="blog-left-control" href="#post-carousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>--}}
+                            {{--<a class="blog-right-control" href="#post-carousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>--}}
+                        {{--</div>--}}
+                        {{--<div class="post-meta">--}}
+                            {{--<span><i class="fa fa-comments-o"></i> 3 Comments</span>--}}
+                            {{--<span><i class="fa fa-heart"></i> 0 Likes</span>--}}
+                        {{--</div>--}}
+                        {{--<div class="post-icon">--}}
+                            {{--<i class="fa fa-picture-o"></i>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="entry-header">--}}
+                        {{--<h3><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h3>--}}
+                        {{--<span class="date">June 26, 2014</span>--}}
+                        {{--<span class="cetagory">in <strong>Photography</strong></span>--}}
+                    {{--</div>--}}
+                    {{--<div class="entry-content">--}}
+                        {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+                {{--<div class="col-sm-4 wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="800ms">--}}
+                    {{--<div class="post-thumb">--}}
+                        {{--<a href="#"><img class="img-responsive" src="images/blog/3.jpg" alt=""></a>--}}
+                        {{--<div class="post-meta">--}}
+                            {{--<span><i class="fa fa-comments-o"></i> 3 Comments</span>--}}
+                            {{--<span><i class="fa fa-heart"></i> 0 Likes</span>--}}
+                        {{--</div>--}}
+                        {{--<div class="post-icon">--}}
+                            {{--<i class="fa fa-video-camera"></i>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+                    {{--<div class="entry-header">--}}
+                        {{--<h3><a href="#">Lorem ipsum dolor sit amet consectetur adipisicing elit</a></h3>--}}
+                        {{--<span class="date">June 26, 2014</span>--}}
+                        {{--<span class="cetagory">in <strong>Photography</strong></span>--}}
+                    {{--</div>--}}
+                    {{--<div class="entry-content">--}}
+                        {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            {{--<div class="load-more wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="500ms">--}}
+                {{--<a href="#" class="btn-loadmore"><i class="fa fa-repeat"></i> Load More</a>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</section><!--/#blog-->--}}
 
 <section id="contact">
     <div id="google-map" class="wow fadeIn" data-latitude="52.365629" data-longitude="4.871331" data-wow-duration="1000ms" data-wow-delay="400ms"></div>
